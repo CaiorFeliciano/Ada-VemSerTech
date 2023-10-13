98c58e07ad60d90f2e55957d29a827d42ab60720
Caio <caiorfeliciano@gmail.com>
2023-10-13 20:55:11 -0300

    created dynamic table

--- Exercício 2 - Formulário/index.js
+++ Exercício 2 - Formulário/index.js
@@ -30,15 +30,46 @@ async function post() {
   }
 }
 
-  async function getUsers() {
-
+async function getUsers() {
+    
   const response = await fetch(
     'http://localhost:3001/users'
   )
     if(response.ok) {
       const data = await response.json()
-      return data
-    }
+      renderTable(data)
+    } 
+}
+
+function renderTable(users) {
+  const tableBody = document.querySelector('#userTable tbody');
+
+  users.forEach(user => {
+    //cria nova row
+    const row = document.createElement('tr');
+    //cria nova células
+    const name = document.createElement('td');
+    const password = document.createElement('td');
+    const email = document.createElement('td');
+    const phonenumber = document.createElement('td');
+    const checkbox = document.createElement('td');
+    //adicionar contúdo as células
+    name.textContent = user.name;
+    password.textContent = user.password;
+    phonenumber.textContent = user.phonenumber
+    email.textContent = user.email
+    checkbox.textContent = user.checkbox;
+    //adionando conteúdo a tabela
+    row.appendChild(name)
+    row.appendChild(password)
+    row.appendChild(email)
+    row.appendChild(phonenumber)
+    row.appendChild(checkbox)
+
+    tableBody.appendChild(row);
+  })
+
+
 }
 
 

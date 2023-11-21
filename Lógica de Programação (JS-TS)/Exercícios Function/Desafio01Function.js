b8d8b605a9adcea818f321908ad207ec9712ca67
Caio <caiorfeliciano@gmail.com>
2023-11-21 18:38:16 -0300

    Function improvements

--- Lógica de Programação (JS-TS)/Exercícios Function/Desafio01Function.js
+++ Lógica de Programação (JS-TS)/Exercícios Function/Desafio01Function.js
@@ -1,9 +1,28 @@
 //EXERCÍCIO 01 
 //Crie uma função que recebe dois números e um operador (+, -, *, /) como parâmetros e retorna o resultado da operação.
 
-function operador(a,b) {
-  return a * b;
+let n1 = 10;
+let n2 = 5;
+let operador = "+"
+
+function calcular(n1, n2, operador) {
+  switch(operador) {
+    case "+":
+      console.log(`A soma dos números é ${n1 + n2}`);
+      break;
+    case "-":
+      console.log(`A subtração dos números é ${n1 - n2}`);
+      break;
+    case "/":
+      console.log(`A divisão dos números é ${n1 / n2}`);
+      break;
+    case "*":
+      console.log(`A multiplicação dos números é ${n1 * n2}`);
+      break;
+    default:
+      console.log("Escolha um operador válido");
+      break;
+  }
 }
 
-console.log(operador(5,5))
-
+calcular(n1,n2,operador)

38a8e2b5650c76c06793d91b5068961827eb8eeb
Caio <caiorfeliciano@gmail.com>
2023-11-21 18:52:35 -0300

    Function Improvements - eval( )

--- Lógica de Programação (JS-TS)/Exercícios Function/Desafio01Function.js
+++ Lógica de Programação (JS-TS)/Exercícios Function/Desafio01Function.js
@@ -6,22 +6,10 @@ let n2 = 5;
 let operador = "+"
 
 function calcular(n1, n2, operador) {
-  switch(operador) {
-    case "+":
-      console.log(`A soma dos números é ${n1 + n2}`);
-      break;
-    case "-":
-      console.log(`A subtração dos números é ${n1 - n2}`);
-      break;
-    case "/":
-      console.log(`A divisão dos números é ${n1 / n2}`);
-      break;
-    case "*":
-      console.log(`A multiplicação dos números é ${n1 * n2}`);
-      break;
-    default:
-      console.log("Escolha um operador válido");
-      break;
+  if (operador === "+" || operador === "-" || operador === "*" || operador === "/") {
+    console.log("O resultado da expressão é " + eval(`${n1} ${operador} ${n2}`))
+  } else {
+    console.log("Operador inválido")
   }
 }
 
